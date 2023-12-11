b1746a67f9dda5e538f86c78e4b55abb12d2f303
Екатерина Савинкова <katy4.savinkova2004@yanedx.ru>
2023-12-12 00:17:47 +0300

    add 3.py

--- 1.py
+++ 1.py
@@ -1,10 +1,9 @@
 import csv
-students = []
 def findVladimir(students):
-  """Outputs a line in the format 'Ты получил: <SCORE>, за проект - <PROJECT_NUM>'
+  """Output a line in the format 'Ты получил: <SCORE>, за проект - <PROJECT_NUM>'
 
     Keyword arguments:
-    students -- list of students
+    students -- List of dictionaries representing students
   """
   for student in students:
     if ((student['name'].split(' ')[0] == 'Хадаров') and (student['name'].split(' ')[1] == 'Владимир')):
@@ -13,10 +12,10 @@ def findVladimir(students):
       print(f'Ты получил: {score}, за проект - {title}')
 
 def findAverage(students):
-  """Returns the average value of students' scores
+  """Return the average value of students' scores
 
     Keyword arguments:
-    students -- list of students
+    students -- List of dictionaries representing students
   """
   col = 0
   sum = 0
@@ -26,21 +25,37 @@ def findAverage(students):
       col += 1
   return round(sum / col, 3)
 
-# считываем строки файла и кладем их в список students
-with open("./student.csv", "r") as file:
-  reader = csv.DictReader(file, delimiter = ',')
-  for student in reader:
-    students.append({'id': student['id'], 'name': student['name'], 'titleProject_id': student['titleProject_id'], 'class': student['class'], 'score': student['score']})
+def readFile(filename):
+  """Read a csv file and return the list of students
+
+    Keyword arguments:
+    filename -- csv file for reading  
+  """
+  students = []
+  with open(filename, "r") as file:
+    reader = csv.DictReader(file, delimiter = ',')
+    for student in reader:
+      students.append({'id': student['id'], 'name': student['name'], 'titleProject_id': student['titleProject_id'], 'class': student['class'], 'score': student['score']})
+  return students
+
+def writeFile(filename, students, average):
+  """Open a csv file and write the modified data
+
+    Keyword arguments:
+    filename -- csv file for writing
+    students -- List of dictionaries representing students
+    average -- average value of students' scores
+  """
+  with open(filename, mode="w", encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
+    file_writer.writerow(["id","name","titleProject_id","class","score"])
+    for student in students:
+      if (student['score'] == 'None'):
+        student['score'] = average
+      file_writer.writerow([student['id'], student['name'], student['titleProject_id'], student['class'], student['score']])
 
 # запускаем выполнение необходимых функций
+students = readFile("student.csv")
 findVladimir(students)
 average = findAverage(students)
-
-# открываем на запись файл student_new.csv и заполняем данными с найденным средним значением оценки студентов
-with open("student_new.csv", mode="w", encoding='utf-8') as w_file:
-  file_writer = csv.writer(w_file, delimiter = ",", lineterminator="\r")
-  file_writer.writerow(["id","name","titleProject_id","class","score"])
-  for student in students:
-    if (student['score'] == 'None'):
-      student['score'] = average
-    file_writer.writerow([student['id'], student['name'], student['titleProject_id'], student['class'], student['score']])
+writeFile("student_new.csv", students, average)
---
+++ 3.py
@@ -0,0 +1,43 @@
+import csv
+def readFile(filename):
+  """Read a csv file and return the list of students
+
+    Keyword arguments:
+    filename -- csv file for reading  
+  """
+  students = []
+  with open(filename, "r") as file:
+    reader = csv.DictReader(file, delimiter = ',')
+    for student in reader:
+      students.append({'id': student['id'], 'name': student['name'], 'titleProject_id': student['titleProject_id'], 'class': student['class'], 'score': student['score']})
+  return students
+
+def findStudentByID(titleProject_id, students):
+  """Find a student by projects's ID from the given list of students
+
+    Keyword arguments:
+    id -- The unique identifier of the student's project to be found
+    students -- List of dictionaries representing students
+  """
+  flag = 0
+  for student in students:
+    if (int(student['titleProject_id']) == int(titleProject_id)):
+      print('Проект № ' + student['titleProject_id'] + ' делал: ' + student['name'].split()[1][0] + '.' + student['name'].split()[0] + '\nОн(а) получил(а) оценку ' + student['score'])
+      flag = 1
+      break
+  if (flag == 0):
+    print('Ничего не найдено')
+
+def SearchingForStudents(students):
+  """Print the main info about student with a project's id, passing to the terminal, and stops when input is the word "СТОП"
+
+  Keyword arguments:
+  students -- List of dictionaries representing students
+  """
+  id = input()
+  while (id != 'СТОП'):
+    findStudentByID(id, students)
+    id = input()
+
+students = readFile("student.csv")
+SearchingForStudents(students)
